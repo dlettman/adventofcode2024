76c4f7e3fb1456d365f77943e57b3c1bad79d15e
dlettman <derek.lettman@gmail.com>
2024-12-02 21:43:36 -0800

    solve day 3

--- 03/exercise_03.py
+++ 03/exercise_03.py
@@ -1,22 +1,51 @@
+import re
 import time
 
 import pyperclip
 
 from helpers import helpers
 
+PATTERN = "mul\\([0-9]+,[0-9]+\\)"
+PATTERN2 = "mul\\(([0-9]+),([0-9]+)\\)"
+
 
 def part_one(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    total = 0
+    for line in puzzle_input:
+        matches = re.findall(PATTERN, line)
+        for match in matches:
+            match2 = re.search(PATTERN2, match)
+            total += int(match2.group(1)) * int(match2.group(2))
+    return total
+
+
+def remove_dont_stuff(line, enabled=True):
+    output = ""
+    for idx in range(len(line)):
+        try:
+            if enabled and line[idx:idx+7] == "don't()":
+                enabled = False
+            elif not enabled and line[idx:idx+4] == "do()":
+                enabled = True
+        except IndexError:
+            pass
+        if enabled:
+            output += line[idx]
+    return output, enabled
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    total = 0
+    enabled = True
+    for line in puzzle_input:
+        do_line, enabled = remove_dont_stuff(line, enabled=enabled)
+        matches = re.findall(PATTERN, do_line)
+        for match in matches:
+            match2 = re.search(PATTERN2, match)
+            total += int(match2.group(1)) * int(match2.group(2))
+    return total
 
 
 if __name__ == "__main__":
