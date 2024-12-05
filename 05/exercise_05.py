3dec44d826345b63def56ec79aa640a0a9246901
dlettman <derek.lettman@gmail.com>
2024-12-04 21:46:46 -0800

    dry it up just a bit

--- 05/exercise_05.py
+++ 05/exercise_05.py
@@ -7,7 +7,7 @@ from helpers import helpers
 
 def get_rules_and_updates(puzzle_input):
     updating = False
-    not_before = {}
+    must_be_before = {}
     updates = []
     for line in puzzle_input:
         if not line:
@@ -15,62 +15,46 @@ def get_rules_and_updates(puzzle_input):
             continue
         elif not updating:
             a, b = line.split("|")
-            if a not in not_before:
-                not_before[a] = {b}
+            if a not in must_be_before:
+                must_be_before[a] = {b}
             else:
-                not_before[a].add(b)
+                must_be_before[a].add(b)
         elif updating:
             updates.append(line.split(','))
-    return not_before, updates
+    return must_be_before, updates
 
+
+def needs_sorting(line, must_be_before):
+    seen = set()
+    for num in line:
+        if num in must_be_before:
+            if must_be_before[num].intersection(seen):
+                return True
+        seen.add(num)
+    return False
 
 def part_one(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    not_before, updates = get_rules_and_updates(puzzle_input)
-    total = 0
-    for line in updates:
-        ok = True
-        seen = set()
-        for num in line:
-            if num in not_before:
-                if not_before[num].intersection(seen):
-                    ok = False
-                    break
-            seen.add(num)
-        if ok:
-            total += int(line[len(line)//2])
-    return total
+    must_be_before, updates = get_rules_and_updates(puzzle_input)
+    return sum([int(line[len(line)//2]) for line in updates if not needs_sorting(line, must_be_before)])
 
 
-def stupid_sort(nums, not_before):
+def stupid_sort(nums, must_be_before):
     seen = set()
     for idx, item in enumerate(nums):
-        if item in not_before:
-            if not_before[item].intersection(seen):
+        if item in must_be_before:
+            if must_be_before[item].intersection(seen):
                 nums.pop(idx)
                 nums.insert(idx - 1, item)
-                return stupid_sort(nums, not_before)
+                return stupid_sort(nums, must_be_before)
         seen.add(item)
     return nums
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    not_before, updates = get_rules_and_updates(puzzle_input)
-    total = 0
-    for line in updates:
-        needs_some_sorting = False
-        seen = set()
-        for num in line:
-            if num in not_before:
-                if not_before[num].intersection(seen):
-                    needs_some_sorting = True
-                    break
-            seen.add(num)
-        if needs_some_sorting:
-            line = stupid_sort(line, not_before)
-            total += int(line[len(line)//2])
-    return total
+    must_be_before, updates = get_rules_and_updates(puzzle_input)
+    return sum([int(stupid_sort(line, must_be_before)[len(line)//2]) for line in updates if needs_sorting(line, must_be_before)])
 
 
 if __name__ == "__main__":
