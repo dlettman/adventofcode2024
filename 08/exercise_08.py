e16efb4a517a9b1a42b92ffc46167893211abace
dlettman <derek.lettman@gmail.com>
2024-12-07 21:49:50 -0800

    solve day 8

--- 08/exercise_08.py
+++ 08/exercise_08.py
@@ -2,21 +2,79 @@ import time
 
 import pyperclip
 
-from helpers import helpers
+import helpers
 
+
+def parse_map(puzzle_input):
+    antennas = {}
+    for y, line in enumerate(puzzle_input):
+        for x, char in enumerate(line):
+            if char != ".":
+                if char in antennas:
+                    antennas[char].add((x, y))
+                else:
+                    antennas[char] = {(x, y)}
+    return antennas
+
+def print_the_thing(antinodes, puzzle_input):
+    for y in range(len(puzzle_input)):
+        line = ""
+        for x in range(len(puzzle_input[0])):
+            if (x, y) in antinodes:
+                line += "#"
+            else:
+                line += "."
+        print(line)
 
 def part_one(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    antinodes = set()
+    antennas = parse_map(puzzle_input)
+    all_antennas = set()
+    for locations in antennas.values():
+        for location in locations:
+            all_antennas.add(location)
+    for char in antennas:
+        for antenna in antennas[char]:
+            for other_antenna in antennas[char]:
+                if not antenna == other_antenna:
+                    dist = (antenna[0] - other_antenna[0], antenna[1] - other_antenna[1])
+                    for ant in [antenna, other_antenna]:
+                        loc = (ant[0] + dist[0], ant[1] + dist[1])
+                        if (0 <= loc[0] <= len(puzzle_input[0]) -1 and (0 <= loc[1] <= len(puzzle_input) -1 )):
+                            print(antenna, " + ", other_antenna, " = ", loc)
+                            if loc not in antennas[char]:
+                                antinodes.add(loc)
+                        loc = (ant[0] - dist[0], ant[1] - dist[1])
+                        if (0 <= loc[0] <= len(puzzle_input[0]) -1 and (0 <= loc[1] <= len(puzzle_input) -1 )):
+                            if loc not in antennas[char]:
+                                antinodes.add(loc)
+    return len(antinodes)
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    antinodes = set()
+    antennas = parse_map(puzzle_input)
+    for char in antennas:
+        for antenna in antennas[char]:
+            for other_antenna in antennas[char]:
+                if not antenna == other_antenna:
+                    dist = (antenna[0] - other_antenna[0], antenna[1] - other_antenna[1])
+                    for ant in [antenna, other_antenna]:
+                        loc = (ant[0] + dist[0], ant[1] + dist[1])
+                        # if becomes while loop...
+                        while (0 <= loc[0] <= len(puzzle_input[0]) -1 and (0 <= loc[1] <= len(puzzle_input) -1 )):
+                            if loc not in antennas[char]:
+                                antinodes.add(loc)
+                            loc = (loc[0] - dist[0], loc[1] - dist[1])
+                        loc = (ant[0] + dist[0], ant[1] + dist[1])
+                        # and again...
+                        while (0 <= loc[0] <= len(puzzle_input[0]) -1 and (0 <= loc[1] <= len(puzzle_input) -1 )):
+                            if loc not in antennas[char]:
+                                antinodes.add(loc)
+                            loc = (loc[0] + dist[0], loc[1] + dist[1])
+    return len(antinodes.union(all_antennas))
 
 
 if __name__ == "__main__":
