7e26c140bda348fb6131c3c28a53c338413fb91b
dlettman <derek.lettman@gmail.com>
2024-12-24 21:37:59 -0800

    solve day 25

--- 25/exercise_25.py
+++ 25/exercise_25.py
@@ -1,22 +1,42 @@
 import time
 
-import pyperclip
-
 from helpers import helpers
 
 
+def get_keys_and_locks(puzzle_input):
+    puzzle_input = puzzle_input.split("\n\n")
+    keys = []
+    locks = dict()
+    for schematic in puzzle_input:
+        schematic = schematic.splitlines()
+        columns = [[row[n] for row in schematic] for n in range(5)]
+        current_dict = keys if columns[0][0] == "#" else locks
+        if current_dict == locks:
+            for column in columns:
+                length = column.count("#")
+                current_dict = current_dict.setdefault(length, {})
+        else:
+            key = [column.count("#") for column in columns]
+            keys.append(key)
+    return keys, locks
+
+
+def find_matches_recursively(current_dict, key, depth=0):
+    if depth == 5:
+        return 1
+    total = 0
+    for n in range(8 - key[depth]):
+        try:
+            total += find_matches_recursively(current_dict[n], key, depth + 1)
+        except KeyError:
+            continue
+    return total
+
+
 def part_one(input_filename):
-    puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
-
-
-def part_two(input_filename):
-    puzzle_input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    puzzle_input = helpers.parse_input(input_filename, split=False)
+    keys, locks = get_keys_and_locks(puzzle_input)
+    return sum([find_matches_recursively(locks, key) for key in keys])
 
 
 if __name__ == "__main__":
@@ -27,15 +47,3 @@ if __name__ == "__main__":
     oneend = time.time()
     print(f"REAL RESULT = {p1result}")
     print(f"Time = {oneend - onestart}")
-    print("\n")
-    print("*** PART TWO ***\n")
-    print(f"Test result = {part_two('inputtest.txt')}\n")
-    twostart = time.time()
-    p2result = part_two("input.txt")
-    twoend = time.time()
-    print(f"REAL RESULT = {p2result}")
-    print(f"Time = {twoend - twostart}")
-    if p1result:
-        pyperclip.copy(p1result)
-    elif p2result:
-        pyperclip.copy(p2result)
